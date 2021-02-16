74837dccb711046ad9cec62d2796f51115ed9423
kbj804 <kbj80494@gmail.com>
2021-02-16 17:33:37 +0900

    read pdf done

--- Scripts/tp/regex/loadFileManager.py
+++ Scripts/tp/regex/loadFileManager.py
@@ -9,36 +9,41 @@ from io import StringIO
 
 class loadFileManager:
     # 파일 이름, 확장자 분리
-    def __init__(self, path) -> None:
+    def __init__(self, path):
         # dir_path = os.getcwd()
-        basename = os.path.basename(path)
+        self.path = path
+        basename = os.path.basename(self.path)
         self.name, self.ext = os.path.splitext(basename)
 
     # 확장자 별 데이터 오픈
     # pdf, hwp, ppt, docx, ....
 
-    def read_pdf(self, path):
+    def read_pdf(self):
         rsrcmgr = PDFResourceManager()
         retstr = StringIO()
         codec = 'utf-8'
         laparams = LAParams()
         
-        f = open('./out.html', 'wb')
-        device = HTMLConverter(rsrcmgr, f, codec=codec, laparams=laparams)
-        # device = TextConverter(rsrcmgr, f, codec=codec, laparams=laparams)
-        fp = open(path, 'rb')
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        password = ""
-        maxpages = 0 #is for all
-        caching = True
-        pagenos=set()
-        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-            interpreter.process_page(page)
-        fp.close()
+        # f = open('./out.html', 'wb')
+        # device = HTMLConverter(rsrcmgr, f, codec=codec, laparams=laparams)
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        
+        with open(self.path, 'rb') as fp:
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            password = ""
+            maxpages = 0 #is for all
+            caching = True
+            pagenos=set()
+            
+            for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+                interpreter.process_page(page)
+            
+            str = retstr.getvalue()
+
+            fp.close()
         device.close()
-        str = retstr.getvalue()
         retstr.close()
-        f.close()
+        # f.close()
         
         return str
 
@@ -62,3 +67,7 @@ class loadFileManager:
         'exel': read_exel
 
     }
+
+a = loadFileManager('t.pdf')
+
+print(a.read_pdf())
